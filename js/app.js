be3a3cfcffff51e43237abc991d29dc571be7622
just9krish <just9krish434@gamil.com>
2022-10-24 23:55:10 +0530

    having trouble with changing the bol value of inside object will fix later

--- js/app.js
+++ js/app.js
@@ -5,37 +5,11 @@ const todoInput = document.querySelector(".todo__form-input");
 const todoList = document.querySelector(".todo__list");
 const toodsCounter = document.querySelector(".todo__menu-counter");
 
-let todos;
-if (localStorage.getItem("savedTod0") == null) {
-  todos = [];
-}
-
-todoForm.addEventListener("submit", addTodo);
-
-function addTodo(event) {
-  event.preventDefault();
-  if (!formValidation(todoInput.value)) return;
-
-  if (todos == null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("savedTodo"));
-  }
-
-  todos.push({
-    text: todoInput.value,
-    done: false,
-  });
-  todoForm.reset();
-  localStorage.setItem("savedTodo", JSON.stringify(todos));
-  renderList();
-  countActiveTodos();
-  console.log(todos);
-}
+let todos = JSON.parse(localStorage.getItem("savedTodo")) || [];
 
 // function to display todo list
 function renderList() {
-  if (localStorage.getItem("savedTodo") === null) {
+  if (localStorage.getItem("savedTodo") == null) {
     todos = [];
   } else {
     todos = JSON.parse(localStorage.getItem("savedTodo"));
@@ -46,7 +20,7 @@ function renderList() {
   todos.forEach((list, index) => {
     htmlCode += `
     <li class="todo__item">
-        <input class="todo__item-input" type="checkbox" />
+        <input id="item${index}" class="todo__item-input" type="checkbox" data-index="${index}" />
         <label class="todo__label" for="item${index}">
             <span class="todo__label-text">${list.text}</span>
             <span class="todo__span"></span>
@@ -61,6 +35,34 @@ function renderList() {
   });
 
   todoList.innerHTML = htmlCode;
+
+  // console.log(document.querySelectorAll(".todo__item"))
+
+  document.querySelectorAll(".todo__item").forEach((item) => {
+    // console.log("clck");
+    addEventListener("click", () => toggleDone(item));
+  });
+}
+
+// function to add todo into lists
+function addTodo(event) {
+  event.preventDefault();
+  if (!formValidation(todoInput.value)) return;
+
+  let todo = {
+    text: todoInput.value,
+    done: false,
+    id: todos.length,
+  };
+
+  todos = JSON.parse(localStorage.getItem("savedTodo")) || [];
+  todos.push(todo);
+
+  todoForm.reset();
+  localStorage.setItem("savedTodo", JSON.stringify(todos));
+  renderList();
+  countActiveTodos();
+  console.log(todos);
 }
 
 // function to remove specific todo
@@ -90,7 +92,15 @@ function formValidation(value) {
 
 // function to dynamically count how many active task is left
 function countActiveTodos() {
-  let savedTodo = JSON.parse(localStorage.getItem("savedTodo"));
+  let savedTodo;
+  if (JSON.parse(localStorage.getItem("savedTodo")) == null) {
+    savedTodo = [];
+    console.log("A");
+  } else {
+    console.log("b");
+    savedTodo = JSON.parse(localStorage.getItem("savedTodo"));
+  }
+
   toodsCounter.innerHTML = `${savedTodo.length} ${
     savedTodo.length == 1 ? "item left" : "items left"
   }`;
@@ -99,5 +109,18 @@ function countActiveTodos() {
   ${savedTodo.length > 0 ? `| (${savedTodo.length})` : ""}`;
 }
 
+// function to done/undone on click
+function toggleDone(item) {
+  console.log(item)
+  let checkbox = item.querySelector(".todo__item-input");
+  let index = checkbox.dataset.index;
+  console.log(index)
+
+  todos[index].done = !todos[index].done;
+  console.log(todos[index].done);
+  console.log(todos[index].text);
+}
+
 countActiveTodos();
 renderList();
+todoForm.addEventListener("submit", addTodo);
