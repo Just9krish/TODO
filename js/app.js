8bf72c0dc80c85b3b859a0d42ef5ccd98a9754ec
just9krish <just9krish434@gamil.com>
2022-10-28 01:27:55 +0530

    added function to tell there is no todo to do

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ function displayTodos(tab = "all") {
   removeAllTodos(todoList);
   countActiveTodos();
 
+  todos.length == 0 ? renderWhileEmpty(tab) : 
+
   todos.forEach((todo, index) => {
     if (tab == "all") {
       renderList(todo, index);
@@ -154,11 +156,18 @@ function removeCompletedTodos() {
   countActiveTodos();
 }
 
-// function checkIfCompleted() {
-//   let completedResult = 0;
+function renderWhileEmpty(tab) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("todo__item", "todo__item-info")
 
-//   todos.forEach((todo) => (todo.done ? completedResult++ : null));
-// }
+  listItem.innerHTML = `<svg class="todo__item-info-icon" xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/></svg> ${
+    tab == "completed"
+      ? "You have no completed todo todo display!"
+      : "You have no active todo todo display!"
+  }`;
+
+  todoList.appendChild(listItem);
+}
 
 // Event listenres
 displayTodos();
