2ef0aef0b3ab687067bfe29201e1c648ba7e40d3
just9krish <just9krish434@gamil.com>
2022-10-30 21:16:47 +0530

    fixes issues of sortable

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import Modal from "./modal.js";
 
 const check = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20"><path d="M243.8 339.8C232.9 350.7 215.1 350.7 204.2 339.8L140.2 275.8C129.3 264.9 129.3 247.1 140.2 236.2C151.1 225.3 168.9 225.3 179.8 236.2L224 280.4L332.2 172.2C343.1 161.3 360.9 161.3 371.8 172.2C382.7 183.1 382.7 200.9 371.8 211.8L243.8 339.8zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>`;
 const error = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20"><path fill-rule="evenodd" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>`;
-const info = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/></svg>`
+const info = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/></svg>`;
 
 const todoForm = document.querySelector(".todo__form");
 const todoInput = document.querySelector(".todo__form-input");
@@ -98,8 +98,8 @@ function formValidation(value) {
     modal.show("Error! Todo cannot be blank", "error", error);
     todoForm.reset();
     return false;
-  } else if (value.length >= 35) {
-    modal.show("Error! Maximum 35 character", "error", error);
+  } else if (value.length >= 40) {
+    modal.show("Error! Maximum 40 character", "error", error);
     todoForm.reset();
     return false;
   } else {
@@ -144,7 +144,7 @@ function toggleDone(e) {
 
 // function to remove completed todo
 function removeCompletedTodos() {
-  if(validateCompletedTodo()) return
+  if (validateCompletedTodo()) return;
   todos = JSON.parse(localStorage.getItem("savedTodo"));
 
   const filteredTodos = todos.filter((todo) => !todo.done);
@@ -207,7 +207,7 @@ function validateCompletedTodo() {
     modal.show("Upps.. No completed todo to clear", "info", info);
     return true;
   } else {
-    modal.show("Success! Todos has been deleted", "success", check)
+    modal.show("Success! Todos has been deleted", "success", check);
   }
 }
 
@@ -224,10 +224,13 @@ let sortable = Sortable.create(todoList, {
     listItems.forEach((todo) => todosId.push(todo.id));
 
     todosId.forEach((id) => {
+      let isFound = false;
       oldTodos.forEach((item, index) => {
         if (id == item.id) {
+          if (isFound) return;
           sortedTodos.push(item);
           oldTodos.splice(index, 1);
+          isFound = true;
         }
       });
     });
@@ -254,4 +257,3 @@ tabs.forEach((tab) =>
     displayTodos(activeTab);
   })
 );
-
